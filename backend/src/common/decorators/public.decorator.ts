77d1a12538d85a86c2f059f8ff347a5a7d831cd9
M. Ikhsan Pasaribu <mikhsanpasaribu@gmail.com>
2025-10-29 21:23:25 +0700

    Add Public decorator to mark routes as public without authentication

---
+++ backend/src/common/decorators/public.decorator.ts
@@ -0,0 +1,8 @@
+import { SetMetadata } from '@nestjs/common';
+
+export const IS_PUBLIC_KEY = 'isPublic';
+
+/**
+ * Decorator untuk menandai route sebagai public (tidak perlu authentication)
+ */
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
