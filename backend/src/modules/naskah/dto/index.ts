930b130f423cac4305221af4487c869e24d117c8
M. Ikhsan Pasaribu <mikhsanpasaribu@gmail.com>
2025-10-29 21:31:03 +0700

    Add index file to export all DTOs for Naskah module

---
+++ backend/src/modules/naskah/dto/index.ts
@@ -0,0 +1,18 @@
+/**
+ * Export semua DTOs untuk Naskah module
+ */
+
+// Buat Naskah
+export * from './buat-naskah.dto';
+
+// Perbarui Naskah
+export * from './perbarui-naskah.dto';
+
+// Filter Naskah (Pagination & Search)
+export * from './filter-naskah.dto';
+
+// Ajukan Naskah untuk Review
+export * from './ajukan-naskah.dto';
+
+// Terbitkan Naskah
+export * from './terbitkan-naskah.dto';
