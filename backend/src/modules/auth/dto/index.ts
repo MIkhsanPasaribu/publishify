8aa29d7eb260169d02a878c32741d55ca551cd80
M. Ikhsan Pasaribu <mikhsanpasaribu@gmail.com>
2025-10-29 21:27:20 +0700

    Add index file to export all DTOs for Authentication module

---
+++ backend/src/modules/auth/dto/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Export semua DTOs untuk Authentication module
+ */
+
+// Daftar (Register)
+export * from './daftar.dto';
+
+// Login
+export * from './login.dto';
+
+// Refresh Token
+export * from './refresh-token.dto';
+
+// Lupa Password
+export * from './lupa-password.dto';
+
+// Reset Password
+export * from './reset-password.dto';
+
+// Verifikasi Email
+export * from './verifikasi-email.dto';
